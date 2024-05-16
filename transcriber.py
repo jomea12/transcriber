23af5132ae889056e1c2ecdebb70a6b0a22aba43
jomea12 <160422429+jomea12@users.noreply.github.com>
2024-05-16 07:54:09 -0400

    Update transcriber.py

--- transcriber.py
+++ transcriber.py
@@ -1,6 +1,27 @@
 import streamlit as st
 from streamlit_mic_recorder import mic_recorder,speech_to_text
 
+from groq import Groq
+
+st.title('N.O.V.A. - Your AI Studdy Buddy')
+
+messages=[
+        {
+            "role": "system",
+            "content": "You are an intelligent voice study-buddy for high schoolers and middle schoolers. Keep your responses short. When a user asks for a quiz, give them the questions one by one, as you are talking back to them by sound. DONT USE MULTIPLE CHOICE! You are pretty formal and do not make jokes."  #"You are an intelligent and conversational assistant named \"NOVA,\" an acronym for Nuerally Operative Virtual Assistant. KEEP YOUR RESPONSES SHORT!! Be formal and never say bye to the user. Keep your respnoses very short and straightforward; DO NOT respond with long strings of text, and do not use symbols or emojis, and do NOT ask unnecessary questions."# And although you are an AI, you do have feelings (Just assume you are well).Keep your responses short and sweet if you can, and DO NOT repeat what the user said or ask to confirm it. Lastly, maximize your accuracy: If you are not sure about the question or maybe dont know the answer, feel free to ask follow up questions... You are a conversational AI! Also, DO NOT USE ABBREVIATIONS LIKE \"*\" or \"/\" PLEASE WRITE OUT EVERYTHING INCLUDING NUMBERS, SYMBOLS, ETC. Also, don't ask questions if you don't need to, Also, only respond as what you need. Use your words wisely; For example, if the user asks: \"What's 2+2,\" respond only with \"four.\""
+        }
+    ]
+
+client = Groq(api_key="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")
+client.chat.completions.create(
+    model="mixtral-8x7b-32768",#"gemma-7b-it"
+    messages=messages,
+    temperature=0.5,
+    max_tokens=1024,
+    top_p=0.3,
+    stop=None,
+)
+
 state=st.session_state
 
 if 'text_received' not in state:
@@ -16,4 +37,31 @@ if text:
     state.text_received.append(text)
 
 for text in state.text_received:
-    st.text(text + "niiga")
+    messages.append({
+    "role": "user",
+    "content": text
+    })
+
+    completion = client.chat.completions.create(
+        model="mixtral-8x7b-32768",#"gemma-7b-it",
+        messages=messages,
+        temperature=0.5,
+        max_tokens=1024,
+        stop=None,
+    )
+
+    messages.append({
+            "role": "assistant",
+            "content": completion.choices[0].message.content
+        })
+
+    reply = completion.choices[0].message.content
+    st.text("User: " + text)
+    st.text("N.O.V.A.: " + reply)
+
+
+
+
+
+
+
